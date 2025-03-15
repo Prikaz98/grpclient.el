2b609a3e7c04c92485e0cec9acf1081d542b9945
Ivan Prikaznov <77636647+Prikaz98@users.noreply.github.com>
2025-03-15 13:08:18 +0100

    init

---
+++ grpclient-mode.el
@@ -0,0 +1,89 @@
+;;; grpclient-mode.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2025 Ivan Prikaznov
+;;
+;; Author: Ivan Prikaznov <prikaznov555@gmail.com>
+;; Maintainer: Ivan Prikaznov <prikaznov555@gmail.com>
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Code:
+
+(defvar grpclient-mode-hook nil)
+
+
+(defvar grpclient-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-c C-v") #'grpclient-send-current)
+    (define-key map (kbd "C-c C-u") #'grpclient-copy-grpcurl-to-clipboard)
+    (define-key map (kbd "C-c C-l") #'grpclient-describe)
+    (define-key map (kbd "<tab>") #'grpclient-toggle-pretty-body)
+    map)
+  "Keymap of grpclient major mode.")
+
+
+(defface grpclient-service-face
+  '((t (:inherit font-lock-variable-name-face)))
+  "Face for HTTP method."
+  :group 'grpclient-faces)
+
+
+(defface grpclient-url-face
+  '((t (:inherit font-lock-function-name-face)))
+  "Face for variable value (Emacs lisp)."
+  :group 'grpclient-faces)
+
+
+(defface grpclient-proto-face
+  '((t (:inherit font-lock-type-face)))
+  "Face for variable value (Emacs lisp)."
+  :group 'grpclient-faces)
+
+
+(defface grpclient-var-name-face
+  '((t (:inherit font-lock-keyword-face)))
+  "Face for variable value (Emacs lisp)."
+  :group 'grpclient-faces)
+
+
+(defface grpclient-var-value-face
+  '((t (:inherit font-lock-string-face)))
+  "Face for variable value (Emacs lisp)."
+  :group 'grpclient-faces)
+
+
+(defconst grpclient-mode-font-lock-keywords
+  (list
+   (list "\\(GRPC\\) \\([^\s\n\r]+\\) \\(.+\\)$"
+         '(1 'grpclient-var-name-face)
+         '(2 'grpclient-url-face)
+         '(3 'grpclient-proto-face))
+   (list "\\(^:[^\s\n]+\\)=\\([^\s\n]+\\)$"
+         '(1 'grpclient-var-name-face)
+         '(2 'grpclient-var-value-face)))
+  "Minimal highlighting grpclient entities.")
+
+
+(defconst grpclient-mode-syntax-table
+  (let ((table (make-syntax-table)))
+    (modify-syntax-entry ?\# "<" table)
+    (modify-syntax-entry ?\n ">#" table)
+    table))
+
+
+;;TODO: fix, it is not working now
+;;;###autoload
+(define-derived-mode grpclient-mode fundamental-mode "GRPClient"
+  "Major mode for editing grpc buffer."
+  (set (make-local-variable 'comment-start) "# ")
+  (set (make-local-variable 'comment-start-skip) "# *")
+  (set (make-local-variable 'comment-column) 48)
+
+  (use-local-map grpclient-mode-map)
+  (set (make-local-variable 'font-lock-defaults) '(grpclient-mode-font-lock-keywords))
+  (run-hooks 'grpclient-mode-hook))
+
+
+(provide 'grpclient-mode)
+
+;;grpclient-mode.el ends here
---
+++ grpclient.el
@@ -0,0 +1,217 @@
+;;; grpclient.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2025 Ivan Prikaznov
+;;
+;; Author: Ivan Prikaznov <prikaznov555@gmail.com>
+;; Maintainer: Ivan Prikaznov <prikaznov555@gmail.com>
+;; Created: 14 Mar 2025
+;; Keywords: grpc grpcurl
+
+;;
+;; This file is not part of GNU Emacs.
+;;
+
+;;; Commentary:
+;; This is tool to manually explore and test GRPC Services based on
+;; https://github.com/fullstorydev/grpcurl project. Runs queries for a
+;; plain-text query sheet, displays results in separated buffer.
+
+;;; TODO:
+;; - hooks
+;; - print message to install grpcurl if it doesn't exists
+
+;;; Code:
+;;
+(require 's)
+(require 'json)
+(require 'dash)
+(eval-when-compile
+  (if (version< emacs-version "26")
+      (require 'cl)
+    (require 'cl-lib)))
+(require 'grpclient-mode)
+
+(defcustom grpclient-default-flags '("-plaintext")
+  "Default flags that append in every query."
+  :type 'list
+  :group 'grpclient)
+
+(defconst grpclient-flags-block-start-regexp "^:flags=<<")
+(defconst grpclient-flags-block-end-regexp "^>>")
+(defconst grpclient-comment-separator "#")
+(defconst grpclient-comment-start-regexp (concat "^" grpclient-comment-separator))
+(defconst grpclient-comment-not-regexp (concat "^[^" grpclient-comment-separator "]"))
+(defconst grpclient-empty-line-regexp "^\\s-*$")
+(defconst grpclient-entities-separator "^$")
+
+
+(defun grpclient--trim-to-nil (str)
+  "If trimmed string is empty return nil else trimmed string"
+  (let ((trimmed (string-trim str)))
+    (unless (string-empty-p trimmed) trimmed)))
+
+
+(defun grpclient--current-min ()
+  (save-excursion
+    (beginning-of-line)
+    (if (looking-at grpclient-comment-start-regexp)
+        (if (or
+             (re-search-forward grpclient-comment-not-regexp (point-max) t)
+             (re-search-forward grpclient-entities-separator (point-max) t))
+            (point-at-bol) (point-max))
+      (if (or
+           (re-search-backward grpclient-comment-start-regexp (point-min) t)
+           (re-search-backward grpclient-entities-separator (point-min) t))
+          (point-at-bol 2)
+        (point-min)))))
+
+
+(defun grpclient--current-max ()
+  (save-excursion
+    (if (or (re-search-forward grpclient-comment-start-regexp (point-max) t)
+            (re-search-forward grpclient-entities-separator (point-max) t))
+        (max (- (point-at-bol) 1) 1)
+      (progn (goto-char (point-max))
+             (if (looking-at "^$") (- (point) 1) (point))))))
+
+
+(defun grpclient--current-line ()
+  (buffer-substring-no-properties
+   (progn (beginning-of-line) (point))
+   (progn (end-of-line) (point))))
+
+
+(defun grpclient--collect-vars-before ()
+  (save-excursion
+    (let ((vars nil)
+          (bound (point)))
+      (goto-char (point-min))
+      (while (search-forward-regexp "^:.+=" bound t)
+        (cl-destructuring-bind (key value) (split-string (grpclient--current-line) "=")
+          (setq vars (cons (cons key value) vars))))
+      vars)))
+
+(defun grpclient--find-flags (bound)
+  (save-excursion
+    (goto-char (point-min))
+    (when (search-forward-regexp grpclient-flags-block-start-regexp bound t)
+      (thread-last
+        (buffer-substring-no-properties
+         (point)
+         (or
+          (search-forward-regexp (concat ">>") bound t)
+          (grpclient--current-max)))
+        (s-replace-regexp (concat grpclient-comment-start-regexp ".+$") "")
+        (s-replace-regexp grpclient-flags-block-end-regexp "")
+        (s-replace-regexp "[ \r\t\n]+" " ")
+        (grpclient--trim-to-nil)))))
+
+
+(defun grpclient--build-command ()
+  (save-excursion
+    (goto-char (grpclient--current-min))
+    (let* ((url-proto-method (cdr (string-split (grpclient--current-line) " ")))
+           (url (first url-proto-method))
+           (vars (grpclient--collect-vars-before))
+           (method (second url-proto-method))
+           (proto (third url-proto-method))
+           (cmax (grpclient--current-max))
+           (flags (grpclient--find-flags (grpclient--current-min)))
+           (body (string-trim (buffer-substring-no-properties (min (point) cmax) cmax)))
+           (cmd-builder (list "grpcurl"
+                              (when body (concat "-d '" (s-replace-all vars body) "'"))
+                              (string-join grpclient-default-flags " ")
+                              (when flags (s-replace-all vars flags))
+                              (when proto (concat "-proto " (s-replace-all vars proto)))
+                              (s-replace-all vars url)
+                              (if method (s-replace-all vars method) ""))))
+      (string-join (-non-nil cmd-builder) " "))))
+
+
+(defun grpclient-pretty-current ()
+  "Pretty print body of current grpclient entity."
+  (interactive)
+  (save-excursion
+    (let ((body-start)
+          (body-end))
+      (when (re-search-forward "^\{\.+\}$" (grpclient--current-max) t)
+        (setq body-end (point))
+        (beginning-of-line)
+        (setq body-start (point))
+        (json-pretty-print body-start body-end)
+        t))))
+
+
+(defun grpclient-collaps-current ()
+  "Collaps body of current grpclient entity."
+  (interactive)
+  (save-excursion
+    (when (re-search-forward "^\{$" (grpclient--current-max) t)
+      (let ((body-start)
+            (body-end))
+        (setq body-start (- (point) 1))
+        (when (re-search-forward "^\}$" (grpclient--current-max))
+          (setq body-end (point))
+          (json-pretty-print body-start body-end t)
+          t)))))
+
+
+(defun grpclient-toggle-pretty-body ()
+  "Pretty print body if it minimalized or conversely."
+  (interactive)
+  (or (grpclient-pretty-current)
+      (grpclient-collaps-current)))
+
+
+(defun grpclient-copy-grpcurl-to-clipboard ()
+  "Build command and copy them into clipboard."
+  (interactive)
+  (let ((cmd (grpclient--build-command)))
+    (kill-new cmd)
+    (message cmd)))
+
+
+(defun grpclient-send-current ()
+  "Prepare grpcurl command and execute it asynchronously.
+
+Entity format:
+#
+GRPC host:port path.Service.Method
+{}
+#
+- Use # or empty lines to set bound of the entity
+- GRPC is an anchor of the query
+- {} is an body of the query.
+
+To exactly ensure what command is built call method
+`(grpclient-copy-grpcurl-to-clipboard)`"
+  (interactive)
+  (let ((cmd (grpclient--build-command)))
+    (with-temp-buffer
+     (async-shell-command cmd "*GRPC Response*" "*GRPC Error*")
+     (buffer-string))))
+
+
+(defvar grpclient-last-url nil)
+
+
+(defun grpclient-describe ()
+  "Describe things."
+  (interactive)
+  (let* ((url (setq grpclient-last-url (read-string "Enter url: " grpclient-last-url)))
+         (message (grpclient--trim-to-nil (read-string "Enter message or nothing: " (thing-at-point 'filename t))))
+         (builder (list
+                   "grpcurl -plaintext"
+                   (when message "-msg-template")
+                   url
+                   "describe"
+                   message))
+         (cmd (string-join (-non-nil builder) " ")))
+    (async-shell-command cmd
+                         (string-join (-non-nil (list "*GRPC" message "Description*")) " ")
+                         "*GRPC Error*")))
+
+
+(provide 'grpclient)
+
+;;grpclient.el  ends here
